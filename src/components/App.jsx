1c072e0e4e55837892c41e412d0be8235683a895
Matias <57120079+elgrantut@users.noreply.github.com>
2020-10-12 18:25:19 -0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DataFetching from './DataFetching2'
+import '../styles.css'
+
+export default function App() {
+  return (
+    <div className="container m-5">
+      <h1 className="text-primary display-4">Data Fetching</h1>
+      <hr />
+      <DataFetching />
+    </div>
+  )
+}
